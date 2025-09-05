43d1a3a4f2f390616da93d366b6470edb43fb74f
maryamabdallah <maryamabdallah698@gmail.com>
2025-09-05 21:46:27 +0300

    custom card

---
+++ lib/core/common/widgets/custom_card.dart
@@ -0,0 +1,20 @@
+import 'package:edutech_app/core/theme/app_colors.dart';
+import 'package:edutech_app/core/theme/app_spacing.dart';
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({super.key, required this.child});
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(AppSpacing.xl),
+      ),
+      elevation: AppSpacing.xs,
+      color: AppColors.sky50,
+      child: child,
+    );
+  }
+}
