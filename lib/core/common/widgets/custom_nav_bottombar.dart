59d9b9805b67787dc04d54a9f16abb127fc4fecc
maryamabdallah <maryamabdallah698@gmail.com>
2025-09-09 01:21:14 +0300

    custom nav bottom bar

---
+++ lib/core/common/widgets/custom_nav_bottombar.dart
@@ -0,0 +1,54 @@
+import 'package:edutech_app/core/common/widgets/nav_item.dart';
+import 'package:edutech_app/core/theme/app_spacing.dart';
+import 'package:flutter/material.dart';
+
+class CustomNavBottombar extends StatelessWidget {
+  const CustomNavBottombar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: AppSpacing.bottomNavHeight,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.white.withOpacity(.1), Colors.white],
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildNavItem(
+            context,
+            index: 0,
+            iconPath: 'assets/images/house.png',
+            label: 'Home',
+          ),
+          buildNavItem(
+            context,
+            index: 1,
+            iconPath: 'assets/images/book.png',
+            label: 'Lessons',
+          ),
+          buildNavItem(
+            context,
+            index: 2,
+            iconPath: 'assets/images/award.png',
+            label: 'Awards',
+          ),
+          buildNavItem(
+            context,
+            index: 3,
+            iconPath: 'assets/images/people.png',
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
