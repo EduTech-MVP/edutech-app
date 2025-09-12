3345eddc0d3882ed32035a047b29694be08f2d53
maryamabdallah <maryamabdallah698@gmail.com>
2025-09-13 02:08:34 +0300

    add views routes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,20 +1,30 @@
+import 'package:edutech_app/features/auth/views/home.dart';
+import 'package:edutech_app/features/auth/views/parent_home.dart';
+import 'package:edutech_app/features/auth/views/splash_screen.dart';
+import 'package:edutech_app/features/auth/views/teacher_home.dart';
 import 'package:flutter/material.dart';
 import '../../features/onboarding/view/onboarding_screen.dart';
-import '../../features/auth/view/sign_up_screen.dart';
-import '../../features/auth/view/sign_in_screen.dart';
+import '../../features/auth/views/sign_up_screen.dart';
+import '../../features/auth/views/sign_in_screen.dart';
 
 class AppRoutes {
   static const String onboarding = '/onboarding';
   static const String signUp = '/signup';
   static const String signIn = '/signin';
+  static const String splashScreen = '/splashscreen';
   static const String home = '/home';
+  static const String parentHome = '/parent';
+  static const String teacherHome = '/teacher';
 
   static Map<String, WidgetBuilder> get routes {
     return {
       onboarding: (context) => const OnboardingScreen(),
       signUp: (context) => const SignUpScreen(),
       signIn: (context) => const SignInScreen(),
-      // home: (context) => const HomeScreen(), // TODO: Create home screen
+      splashScreen: (context) => const SplashScreen(),
+      home: (context) => const Home(),
+      parentHome: (context) => const ParentHome(),
+      teacherHome: (context) => const TeacherHome(),
     };
   }
 }
