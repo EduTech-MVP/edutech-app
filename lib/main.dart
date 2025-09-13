0c08ea46f72a0e46599eef38013a3dfe7f82e594
Tarik-Lotfy <tareklotfy@std.mans.edu.eg>
2025-09-13 18:10:05 +0300

    added providers in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:edutech_app/features/student/controllers/navigation_controller.d
 import 'package:edutech_app/features/student/controllers/task_list_controller.dart';
 import 'package:edutech_app/features/student/controllers/student_classes_controller.dart';
 import 'package:edutech_app/features/parent/controller/parent_navigation_controller.dart';
+import 'package:edutech_app/features/teacher/controller/teacher_navigation_controller.dart';
+import 'package:edutech_app/features/teacher/controller/teacher_classes_controller.dart';
+import 'package:edutech_app/features/teacher/controller/teacher_students_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'core/theme/app_theme.dart';
@@ -12,10 +15,20 @@ void main() {
   runApp(
     MultiProvider(
       providers: [
+        // Student providers
         ChangeNotifierProvider(create: (_) => NavigationController()),
         ChangeNotifierProvider(create: (_) => TaskListController()),
         ChangeNotifierProvider(create: (_) => StudentClassesController()),
+
+        // Parent providers
         ChangeNotifierProvider(create: (_) => ParentNavigationController()),
+
+        // Teacher providers
+        ChangeNotifierProvider(create: (_) => TeacherNavigationController()),
+        ChangeNotifierProvider(create: (_) => TeacherClassesController()),
+        ChangeNotifierProvider(create: (_) => TeacherStudentsController()),
+
+        // Common providers
         ChangeNotifierProvider(create: (_) => OnboardingProvider()),
       ],
       child: const EduTechApp(),
@@ -32,7 +45,7 @@ class EduTechApp extends StatelessWidget {
       title: 'EduTech',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      initialRoute: AppRoutes.signUp,
+      initialRoute: AppRoutes.teacherMainScreen,
       routes: AppRoutes.routes,
     );
   }
