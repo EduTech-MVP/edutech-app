4e4e2a89d6432db1345a10801f5a2be39c2a739c
Tarik-Lotfy <tareklotfy@std.mans.edu.eg>
2025-12-02 04:36:48 +0200

    refactor: Update typography styles

--- lib/core/theme/app_typography.dart
+++ lib/core/theme/app_typography.dart
@@ -5,7 +5,7 @@ class AppTypography {
   // Update to use Fredoka
   static TextStyle get heading1 => TextStyle(
     fontFamily: 'Fredoka',
-    fontSize: 48,
+    fontSize: 32,
     fontWeight: FontWeight.bold,
     height: 1.0,
     letterSpacing: -0.012,
@@ -14,8 +14,8 @@ class AppTypography {
 
   static TextStyle get heading2 => TextStyle(
     fontFamily: 'Fredoka',
-    fontSize: 30,
-    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
     height: 1.2,
     letterSpacing: -0.0075,
     color: AppColors.textPrimary,
@@ -23,8 +23,8 @@ class AppTypography {
 
   static TextStyle get heading3 => TextStyle(
     fontFamily: 'Fredoka',
-    fontSize: 24,
-    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
     height: 1.33,
     letterSpacing: -0.006,
     color: AppColors.textPrimary,
@@ -32,10 +32,103 @@ class AppTypography {
 
   static TextStyle get heading4 => TextStyle(
     fontFamily: 'Fredoka',
-    fontSize: 20,
-    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
     height: 1.4,
     letterSpacing: -0.005,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get heading5 => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    height: 1.4,
+    letterSpacing: -0.005,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get heading6 => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    height: 1.4,
+    letterSpacing: -0.005,
+    color: AppColors.textPrimary,
+  );
+
+  //XL
+
+  static TextStyle get xxxxl => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 72,
+    fontWeight: FontWeight.w700,
+    height: 1.16,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get xxxl => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 64,
+    fontWeight: FontWeight.w700,
+    height: 1.125,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get xxl => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 56,
+    fontWeight: FontWeight.w700,
+    height: 1.4,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get xl => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 48,
+    fontWeight: FontWeight.w700,
+    height: 1.6,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  //label
+  static TextStyle get labelxl => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    height: 1.375,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get labellarge => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    height: 1.42,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get labelmedium => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    height: 1.5,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get labelsmall => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 11,
+    fontWeight: FontWeight.w700,
+    height: 1.45,
+    letterSpacing: 0,
     color: AppColors.textPrimary,
   );
 
@@ -47,6 +140,25 @@ class AppTypography {
     height: 1.4,
   );
 
+  //caption
+  static TextStyle get caption => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    height: 1.4,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  //overline
+  static TextStyle get overline => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 10,
+    fontWeight: FontWeight.w600,
+    height: 1.6,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
   // Blockquote
   static TextStyle get blockquote => TextStyle(
     fontFamily: 'Fredoka',
@@ -99,24 +211,80 @@ class AppTypography {
     height: 2.8,
     color: AppColors.textPrimary,
   );
-  //Large and Small
+
+  //Large, medium  and Small
   static TextStyle get large => TextStyle(
     fontFamily: 'Fredoka',
-    fontSize: 18,
-    fontWeight: FontWeight.w600,
-    height: 2.8,
+    fontSize: 40,
+    fontWeight: FontWeight.w700,
+    height: 1.2,
     color: AppColors.textBlack,
   );
+
+  static TextStyle get medium => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    height: 1.25,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
   static TextStyle get small => TextStyle(
     fontFamily: 'Fredoka',
-    fontSize: 14,
-    fontWeight: FontWeight.w500,
-    height: 1.4,
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    height: 1.28,
     color: AppColors.textBlack, //change this
   );
 
-  //  Subtle Text
+  //body
+  static TextStyle get bodyxl => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
 
+  static TextStyle get bodylarge => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get bodymedium => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get bodysmall => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  static TextStyle get bodyxs => TextStyle(
+    fontFamily: 'Fredoka',
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+    height: 1.45,
+    letterSpacing: 0,
+    color: AppColors.textPrimary,
+  );
+
+  //  Subtle Text
   static TextStyle get subtle => TextStyle(
     fontFamily: 'Fredoka',
     fontSize: 14,
